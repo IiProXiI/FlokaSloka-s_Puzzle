4c6acc22b20c515641312a94028c79ae4a81eefe
IiProXiI <professorr4b@gmail.com>
2025-09-28 03:07:19 +0300

    Update encryption.js

--- js/encryption.js
+++ js/encryption.js
@@ -1,1 +1,34 @@
+class Encryption {
+    static encrypt(text, key = 'hack2024') {
+        let result = '';
+        for (let i = 0; i < text.length; i++) {
+            const charCode = text.charCodeAt(i) ^ key.charCodeAt(i % key.length);
+            result += String.fromCharCode(charCode);
+        }
+        return btoa(result);
+    }
 
+    static decrypt(encryptedText, key = 'hack2024') {
+        try {
+            const text = atob(encryptedText);
+            let result = '';
+            for (let i = 0; i < text.length; i++) {
+                const charCode = text.charCodeAt(i) ^ key.charCodeAt(i % key.length);
+                result += String.fromCharCode(charCode);
+            }
+            return result;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    static generateHash(text) {
+        let hash = 0;
+        for (let i = 0; i < text.length; i++) {
+            const char = text.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash = hash & hash;
+        }
+        return hash.toString(16);
+    }
+}
